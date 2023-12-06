45adce433d5b618583a9f887b161606492bfe3d6
Youssef-S-Negm <youssef.negm2001@gmail.com>
2023-12-06 22:24:13 +0200

    Added Ball model

---
+++ src/Model/Ball.java
@@ -0,0 +1,17 @@
+package Model;
+
+import javax.media.opengl.GL;
+import java.awt.Point;
+
+public class Ball {
+    public Point point;
+    public Point center;
+
+    public Ball(Point point) {
+        this.point = point;
+    }
+
+    public void drawBall(GL gl, Point point, int texture) {
+        //TODO
+    }
+}
