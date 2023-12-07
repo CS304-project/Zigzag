ab4e6e5ee0da5b5e7ca63b02333b2facf8a10fdf
muhammedhamed <96890824+muhammedhamed@users.noreply.github.com>
2023-12-08 00:36:19 +0200

    random generation of diamond

--- src/Model/Cube.java
+++ src/Model/Cube.java
@@ -12,8 +12,8 @@ public class Cube {
     public Point2D.Double botLeft;
     public Point2D.Double botRight;
     public Point2D.Double centerTileP;
-    boolean hasADiamond;
     public Cube nextCube;
+    public Diamond diamond;
 
     public Cube(Point2D.Double topMid,
                 Point2D.Double topLeft,
@@ -27,15 +27,20 @@ public class Cube {
         this.botMid = new Point2D.Double(centralMid.x, centralMid.y - 0.3);
         this.botLeft = new Point2D.Double(topLeft.x, topLeft.y - 0.3);
         this.botRight = new Point2D.Double(topRight.x, topRight.y - 0.3);
+        generateDiamond();
     }
-
-    public void drawCube(GL gl) {
+    public void drawCube(GL gl, int texture) {
         drawTile(gl);
         drawRProjection(gl);
         drawLProjection(gl);
+        if (diamond != null) {
+            diamond.drawDiamond(gl, texture);
+            diamond.animateDiamond();
+        }
+
     }
 
-    public void drawTile(GL gl){
+    public void drawTile(GL gl) {
         gl.glColor3f(0.572f, 0.929f, 1);
         gl.glBegin(gl.GL_QUADS);
         gl.glVertex2d(topMid.x, topMid.y);
@@ -45,7 +50,7 @@ public class Cube {
         gl.glEnd();
     }
 
-    public void drawRProjection(GL gl){
+    public void drawRProjection(GL gl) {
         gl.glColor3f(0.298f, 0.522f, 0.784f);
         gl.glBegin(gl.GL_QUADS);
         gl.glVertex2d(centralMid.x, centralMid.y);
@@ -55,7 +60,7 @@ public class Cube {
         gl.glEnd();
     }
 
-    public void drawLProjection(GL gl){
+    public void drawLProjection(GL gl) {
         gl.glColor3f(0.250f, 0.431f, 0.635f);
         gl.glBegin(gl.GL_QUADS);
         gl.glVertex2d(centralMid.x, centralMid.y);
@@ -66,8 +71,16 @@ public class Cube {
     }
 
     public void generateDiamond() {
+        int randomNumber = (int) (Math.random() * 10);
+        if (randomNumber > 6) {
+            diamond = new Diamond(
+                    new Point2D.Double((topLeft.x + centralMid.x) / 2, (topLeft.y + centralMid.y) / 2),
+                    new Point2D.Double((centralMid.x + topRight.x) / 2, (centralMid.y + topRight.y) / 2),
+                    new Point2D.Double((topRight.x + topMid.x) / 2, (topRight.y + topMid.y) / 2),
+                    new Point2D.Double((topMid.x + topLeft.x) / 2, (topMid.y + topLeft.y) / 2)
+            );
+        }
 
-        // TODO
     }
 
     public void generateNewCube() {
@@ -81,15 +94,14 @@ public class Cube {
                     new Point2D.Double(topMid.x + horizontalD, topMid.y),
                     new Point2D.Double(topRight.x, topRight.y)
             );
-        } else if (topRight.x + horizontalD >= 1){
+        } else if (topRight.x + horizontalD >= 1) {
             nextCube = new Cube(
                     new Point2D.Double(topLeft.x, topLeft.y + verticalD),
                     new Point2D.Double(topMid.x - horizontalD, topMid.y),
                     new Point2D.Double(topMid.x, topMid.y),
                     new Point2D.Double(topLeft.x, topLeft.y)
             );
-        }
-        else if (randomNumber < 10) {
+        } else if (randomNumber < 10) {
             nextCube = new Cube(
                     new Point2D.Double(topRight.x, topRight.y + verticalD),
                     new Point2D.Double(topMid.x, topMid.y),
@@ -104,8 +116,10 @@ public class Cube {
                     new Point2D.Double(topLeft.x, topLeft.y)
             );
         }
+
     }
-    public void animateCube(){
+
+    public void animateCube() {
         topMid.y -= 0.001;
         topLeft.y -= 0.001;
         centralMid.y -= 0.001;
@@ -114,4 +128,5 @@ public class Cube {
         botLeft.y -= 0.001;
         botRight.y -= 0.001;
     }
+
 }
