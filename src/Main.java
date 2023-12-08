27ec4961006bea9ea0e8e1c630fdba5d17095ce7
KhaledAlaa12 <khaledalaa1055@gmail.com>
2023-12-09 00:59:57 +0200

    add counter for score

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 import com.sun.opengl.util.FPSAnimator;
 
 import javax.media.opengl.GLCanvas;
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main extends JFrame  {
     private final GLCanvas glCanvas;
     private final ZigzagGLEventListener listener;
     private FPSAnimator animator;
-
+    public final JLabel counterLabel;
+    public final JLabel scoreLabel;
+    private final JPanel jPanel;
 
     public Main() {
         super("Zigzag");
@@ -15,12 +18,20 @@ public class Main extends JFrame  {
         glCanvas = new GLCanvas();
         listener = new ZigzagGLEventListener();
         animator = new FPSAnimator(glCanvas, 265);
+        counterLabel = new JLabel("0");
+        scoreLabel = new JLabel("score: ");
+        jPanel = new JPanel();
+        jPanel.add(scoreLabel);
+        jPanel.add(counterLabel);
+        jPanel.setOpaque(true);
+        listener.setCounterLabel(counterLabel);
 
         listener.setGLCanvas(glCanvas);
         listener.setAnimator(animator);
         glCanvas.addGLEventListener(listener);
         glCanvas.addMouseListener(listener);
         add(glCanvas);
+        add(jPanel, BorderLayout.EAST);
         setLocationRelativeTo(this);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(1280, 800);
