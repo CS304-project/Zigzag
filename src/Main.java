ba35f83e27a387d65d30e9bc52d1945a9f91e4c7
Youssef-S-Negm <youssef.negm2001@gmail.com>
2023-12-08 22:39:20 +0200

    moved animator to Main class

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,25 @@
+import Model.Cube;
 import com.sun.opengl.util.FPSAnimator;
 
 import javax.media.opengl.GLCanvas;
 import javax.swing.JFrame;
+import java.awt.geom.Point2D;
 
 public class Main extends JFrame  {
     private final GLCanvas glCanvas;
     private final ZigzagGLEventListener listener;
+    private FPSAnimator animator;
+
 
     public Main() {
         super("Zigzag");
 
         glCanvas = new GLCanvas();
         listener = new ZigzagGLEventListener();
+        animator = new FPSAnimator(glCanvas, 60);
 
         listener.setGLCanvas(glCanvas);
+        listener.setAnimator(animator);
         glCanvas.addGLEventListener(listener);
         glCanvas.addMouseListener(listener);
         add(glCanvas);
@@ -22,9 +28,18 @@ public class Main extends JFrame  {
         setSize(1280, 800);
         setLocationRelativeTo(this);
         setVisible(true);
+        animator.start();
     }
 
     public static void main(String[] args) {
         new Main();
+//        Cube firstCube = new Cube(
+//                new Point2D.Double(0, 0.3),
+//                new Point2D.Double(-0.3, 0),
+//                new Point2D.Double(0.3, 0),
+//                new Point2D.Double(0, -0.3)
+//        );
+//
+//        System.out.println(firstCube.isInside(new Point2D.Double(0.4, 0.0)));
     }
 }
