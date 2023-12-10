1e4a48eb728a68ac2ffa430faaf11fd4fc3a4df4
jonathan ashraf <jonathan.ashraf@yahoo.com>
2023-12-10 17:15:45 +0200

    Create Sound model

---
+++ src/Model/Sound.java
@@ -0,0 +1,26 @@
+package Model;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import javax.sound.sampled.*;
+public class Sound {
+    File file ;
+    AudioInputStream audioStream;
+    Clip clip ;
+
+    public Sound(String path) throws LineUnavailableException, UnsupportedAudioFileException, IOException {
+        file = new File(path);
+        audioStream = AudioSystem.getAudioInputStream(file);
+        clip = AudioSystem.getClip();
+        clip.open(audioStream);
+    }
+    public void Start() {
+        clip.start();
+    }
+    public void Reset() {
+        clip.setMicrosecondPosition(0);
+    }
+    public void Stop() {
+        clip.stop();
+    }
+}
